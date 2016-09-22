07e273cdf4f98cb94147914f88519ed43b586a55
Benjamin Schenker <benjamin.schenker@ge.com>
2016-09-21 22:20:53 -0500

    Add Bounce

--- src/reducer.js
+++ src/reducer.js
@@ -11,7 +11,17 @@ export default function(state = Map(), action) {
   case 'TICK':
     const oldPosition = state.getIn(['position']);
     const speed = state.getIn(['speed']);
-    return setState(state, {position: oldPosition + speed});
+    const direction = state.getIn(['direction']);
+    const newPosition = oldPosition + speed * direction;
+    let newDirection = direction;
+    if (newPosition <= 0 || newPosition >= 600) {
+      newDirection *= -1;
+    }
+    const newState = {
+      position: newPosition,
+      direction: newDirection,
+    }
+    return setState(state, newState);
   default:
   }
   return state;
