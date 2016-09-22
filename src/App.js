cfa2f61fd967714d87ef3a896c3fb63c1822ecf6
Benjamin Schenker <benjamin.schenker@ge.com>
2016-09-21 21:03:55 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import BouncingBox from './BouncingBox/component';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BouncingBox/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/BouncingBox/component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './component.css';
+ 
+class BouncingBox extends Component {
+  render() {
+    return <div className="bouncing-box" />;
+  }
+}
+ 
+export default BouncingBox; // Don’t forget to use export default!
